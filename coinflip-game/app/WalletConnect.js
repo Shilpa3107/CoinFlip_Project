aa4b50d7e9f33abeddab424021db2625e2e4ebf7
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-08-24 07:14:34 +0530

    Set up the wallent connection

---
+++ coinflip-game/app/WalletConnect.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
+
+export default function WalletConnect() {
+  const [account, setAccount] = useState(null);
+
+  const connectWallet = async () => {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      const web3 = new Web3(provider);
+      const accounts = await web3.eth.requestAccounts();
+      setAccount(accounts[0]);
+    } else {
+      alert('MetaMask not found! Please install MetaMask.');
+    }
+  };
+
+  return (
+    <div>
+      {!account ? (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      ) : (
+        <div>Connected Account: {account}</div>
+      )}
+    </div>
+  );
+}
